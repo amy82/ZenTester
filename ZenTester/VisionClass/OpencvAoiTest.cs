952ac97fa6d6d0da369f8f3c69bd1b9f6fe515f6
toripapa <kkoma24@gmail.com>
2025-05-02 20:05:05 +0900

    250502 in aps CenterFineTopCamera 에서 찌그러짐 구현

--- ZenTester/VisionClass/OpencvAoiTest.cs
+++ ZenTester/VisionClass/OpencvAoiTest.cs
@@ -55,26 +55,43 @@ namespace ZenHandler.VisionClass
             //
             //
             //
+            int view = 3;
             // 2. Threshold (밝은 점을 강조)
             Mat binary = new Mat();
+            var blurred = new Mat();
+            var edges = new Mat();
             //Cv2.Threshold(gray, binary, 140, 255, ThresholdTypes.Binary);
-
-
-            int blockSize = 35;// 21; // 반드시 홀수
-            int C = 9;
+            Cv2.GaussianBlur(gray, blurred, new OpenCvSharp.Size(5, 5), 1.2);
+            Cv2.Canny(blurred, edges, 190, 75);  // 윤곽 강화
+            if (view > 0)
+            {
+                Cv2.NamedWindow("Detected edges ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
+                Cv2.ImShow("Detected edges ", edges);
+                Cv2.WaitKey(0);
+            }
+            
+            //Cv2.EqualizeHist(gray, gray);
+            int blockSize = 13;// 21; // 반드시 홀수
+            int C = 13;
             //Cv2.Threshold(blurred, binary, 130, 255, ThresholdTypes.Binary); // 배경 밝기에 따라 Binary 또는 BinaryInv
             //Cv2.Threshold(blurred, binary, (mean.Val0 / 2.5), 255, ThresholdTypes.Binary); // 배경 밝기에 따라 Binary 또는 BinaryInv
             //Cv2.Threshold(blurred, binary, (255 - mean.Val0), 255, ThresholdTypes.BinaryInv); // 배경 밝기에 따라 Binary 또는 BinaryInv
-            Cv2.AdaptiveThreshold(gray, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, blockSize, C);
+            Cv2.AdaptiveThreshold(edges, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, blockSize, C);
             //Cv2.Threshold(blurred, binary, 130, 255, ThresholdTypes.Binary); // 배경 밝기에 따라 Binary 또는 BinaryInv
             //Cv2.Threshold(gray, binary, mean.Val0 / 1.1, 255, ThresholdTypes.BinaryInv); // 배경 밝기에 따라 Binary 또는 BinaryInv
+            if (view > 1)
+            {
+                Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(7, 7));
+                Cv2.MorphologyEx(binary, binary, MorphTypes.Close, kernel);     //끊어졌거나 희미한 외곽선을 연결
+                Cv2.NamedWindow("Detected binary ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
+                Cv2.ImShow("Detected binary ", binary);
+                Cv2.WaitKey(0);
+            }
+                
 
-            Cv2.NamedWindow("Detected binary ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
-            Cv2.ImShow("Detected binary ", binary);
-            Cv2.WaitKey(0);
 
             // 3. Contours 찾기
-            Cv2.FindContours(binary, out OpenCvSharp.Point[][] contours, out _, RetrievalModes.List, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(binary, out OpenCvSharp.Point[][] contours, out _, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
 
             // 4. 이미지 중심 계산
             ///OpenCvSharp.Point center = new OpenCvSharp.Point(gray.Width / 2, gray.Height / 2);
@@ -84,6 +101,11 @@ namespace ZenHandler.VisionClass
 
             // 3. 각 컨투어 중심을 평균
             List<Point2d> centers = new List<Point2d>();
+
+            Mat colorView = new Mat();
+            Cv2.CvtColor(gray, colorView, ColorConversionCodes.GRAY2BGR);
+
+            
             foreach (var contour in contours)
             {
                 Moments M = Cv2.Moments(contour);
@@ -93,7 +115,7 @@ namespace ZenHandler.VisionClass
                 double area = Cv2.ContourArea(contour);
                 double perimeter = Cv2.ArcLength(contour, true);
 
-                if (perimeter == 0) continue; // 나누기 에러 방지
+                //if (perimeter == 0) continue; // 나누기 에러 방지
 
 
 
@@ -103,14 +125,43 @@ namespace ZenHandler.VisionClass
                 float radius;
                 Cv2.MinEnclosingCircle(contour, out center, out radius);
 
-                if (circularity > 0.15 && radius > 300 && area > 100000 && radius < 1300 ) // 원형이고 일정 크기 이상
+                if (circularity > 0.55 && radius > 349  && radius < 1500) // 원형이고 일정 크기 이상 && area > 1000
                 {
-                    Cv2.Circle(gray, (OpenCvSharp.Point)center, (int)radius, Scalar.Blue, 2);
+                    //Cv2.Circle(gray, (OpenCvSharp.Point)center, (int)radius, Scalar.Blue, 3);
+                    // 컬러 이미지에 원을 그림
+                    Cv2.Circle(colorView, (OpenCvSharp.Point)center, (int)radius, Scalar.Blue, 5);
+                    if (radius > 800)
+                    {
+                        foreach (var point in contour)
+                        {
+                            // 중심으로부터 거리 계산
+                            float dx = point.X - center.X;
+                            float dy = point.Y - center.Y;
+                            float dist = (float)Math.Sqrt(dx * dx + dy * dy);
+                            Console.Write("[CenterFind] dist - radius: x = {0:0.00}\n", (dist - radius));
+                            // 기준 반지름과 비교
+                            if (Math.Abs(dist - radius) <= 10.0)
+                            {
+                                Cv2.Circle(colorView, new OpenCvSharp.Point(point.X, point.Y), 3, Scalar.Yellow, -1);
+                            }
+                            else
+                            {
+                                Cv2.Circle(colorView, new OpenCvSharp.Point(point.X, point.Y), 3, Scalar.Red, -1);
+                            }
+                                
+                        }
+                    }
                     centers.Add(new Point2d(center.X , center.Y));
                     Console.Write("[CenterFind] measured circle: x = {0:0.00}, y = {1:0.00}, circularity = {2:0.00}, radius = {3:0.00}, area = {4:0.00}\n", center.X, center.Y, circularity, radius, area);
                 }
             }
-
+            if (view > 2)
+            {
+                Cv2.NamedWindow("Detected colorView ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
+                Cv2.ImShow("Detected colorView ", colorView);
+                Cv2.WaitKey(0);
+            }
+                
             // 평균 좌표 계산
             if (centers.Count > 0)
             {
@@ -126,17 +177,17 @@ namespace ZenHandler.VisionClass
 
                 Rectangle m_clRect = new Rectangle((int)(avgCenter.X - (50)), (int)(avgCenter.Y - (50)), (int)(50 * 2), (int)(50 * 2));
                 Globalo.visionManager.milLibrary.DrawOverlayCircle(index, m_clRect, Color.Yellow, 3, System.Drawing.Drawing2D.DashStyle.Solid);
-                Cv2.Circle(gray, bestPoint, 5, new Scalar(0, 255, 0), 2);
-                Cv2.NamedWindow("Detected gray ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
-                Cv2.ImShow("Detected gray ", gray);
-                Cv2.WaitKey(0);
+                //Cv2.Circle(gray, bestPoint, 5, new Scalar(0, 255, 0), 2);
+                //Cv2.NamedWindow("Detected gray ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
+                //Cv2.ImShow("Detected gray ", gray);
+                //Cv2.WaitKey(0);
             }
-            Rect keyRoi = new Rect((int)2190 + 250, (int)1429 + 250, 650, 650);
-            Mat roiKeyMat = gray[keyRoi];
-            Cv2.NamedWindow("Detected roiKeyMat ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
-            Cv2.ImShow("Detected roiKeyMat ", roiKeyMat);
-            Cv2.WaitKey(0);
-            bool keyRtn = MilKeyCheck(roiKeyMat);
+            //Rect keyRoi = new Rect((int)2190 + 250, (int)1429 + 250, 650, 650);
+            //Mat roiKeyMat = gray[keyRoi];
+            //Cv2.NamedWindow("Detected roiKeyMat ", WindowFlags.Normal);  // 수동 크기 조정 가능 창 생성
+            //Cv2.ImShow("Detected roiKeyMat ", roiKeyMat);
+            //Cv2.WaitKey(0);
+            //bool keyRtn = MilKeyCheck(roiKeyMat);
 
             int centerX = gray.Cols / 2;
             int centerY = gray.Rows / 2;
